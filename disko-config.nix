890f33857a08b9c65ce59160e09e3c9b9cfc7f43
mht-source <mahit_pl@hotmail.com>
2024-10-01 01:10:36 +0200

    Removed free spaces and add new comments in eng

--- disko-config.nix
+++ disko-config.nix
@@ -1,62 +1,36 @@
 {
-
   disko.devices = {
 	disk = {
 	    my-disk = {
-
 	      device = "/dev/sda";
-
 	      type = "disk";
-
 	      content = {
-
 	        type = "gpt";
-
 	        partitions = {
-
 	          efi = {
-
-	            type = "EF00";  # EFI partycja
-
-	            size = "1G";  # Rozmiar 1GB
-
+	            type = "EF00";  # EFI partition
+	            size = "1G";  # Size 1GB
 	            content = {
-
 	              type = "filesystem";
-
 	              format = "vfat";
-
 	              mountpoint = "/boot";
-		      
 		      extraArgs = ["-nboot"]; # Add label boot
-
 	            };
-
 	          };
 
 	          swap = {
-
-	            size = "8G";  # Partycja swap 8 GB
-
+	            size = "8G";  # Swap partition 8 GB
 	            content = {
-
 	              type = "swap";
 		      extraArgs = ["-Lswap"];
-
 	            };
-
 	          };
 
 	          root = {
-
-	            size = "100%";  # Reszta wolnego miejsca na root
-
+	            size = "100%";  # Rest of free space for root
 	            content = {
-
 	              type = "filesystem";
-
 	              format = "ext4";
-
 	              mountpoint = "/";
  	              extraArgs = ["-Lnixos"]; # Add label nixos
 

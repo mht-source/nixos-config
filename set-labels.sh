37b9ef08cff9aa42cee688371854daa921917cbb
mht-source <mahit_pl@hotmail.com>
2024-10-01 00:42:32 +0200

    Removed obsolete lines of code

--- set-labels.sh
+++ set-labels.sh
@@ -6,8 +6,8 @@ DISKO_CONFIG="/root/disko-config.nix"
 # Install required packages if they are not installed
 echo "Checking and installing required packages..."
 
-# Install dosfstools (for fatlabel) and e2fsprogs (for e2label)
-sudo nix-env -iA nixos.dosfstools nixos.e2fsprogs
+# Install disko
+sudo nix-env -iA nixos.disko
 
 # Run disko
 echo "Running disko..."
@@ -16,34 +16,6 @@ sudo nix --experimental-features "nix-command flakes" run github:nix-community/d
 # Check if disko finished successfully
 if [ $? -eq 0 ]; then
     echo "Disko completed successfully."
-
-    # Unmount root partition before setting label
-    echo "Unmounting root partition..."
-    sudo umount -l /dev/sda3
-
-    # Set label for ROOT partition
-    echo "Setting label for ROOT partition..."
-    sudo e2label /dev/sda3 nixos
-
-    #SWAP partition: Disable swap, set label and enable swap
-    echo "Turning off swap..."
-    sudo swapoff /dev/sda2
-
-    # Remount the root partition
-    echo "Mounting root partition..."
-    sudo mount /dev/sda3 /mnt
-
-    # SWAP partition: Initialize swap and set label
-    echo "Setting label for SWAP partition..."
-    sudo mkswap -L swap /dev/sda2
-    sudo swapon /dev/sda2	
-
-
-    # EFI partition: Set label without formatting
-    echo "Setting label for EFI partition..."
-    sudo fatlabel /dev/sda1 boot
-
-    echo "Labels have been successfully assigned."
 else
     echo "Error occurred during disko execution."
 fi

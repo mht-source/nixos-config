a830669bae9452c9635ac24450503ed14e0476b3
mht-source <mahit_pl@hotmail.com>
2024-10-01 01:26:50 +0200

    Added download of disko-config.nix file from repository

--- set-labels.sh
+++ set-labels.sh
@@ -1,13 +1,36 @@
 #!/bin/bash
 
+#URL for config file
+FILE_URL="https://raw.githubusercontent.com/mht-source/nixos-config/refs/heads/main/disko-config.nix"
+
+# Path for save configuration file from github
+DESTINATION_PATH="/root"
+
 # Path to the disko configuration file
 DISKO_CONFIG="/root/disko-config.nix"
+
+#File name
+FILENAME="disko-config.nix"
+
+# Dowload file to path
+echo "Downloading $FILENAME to $DESTINATION_PATH..."
+curl -L -o "DESTINATION_PATH/$FILENAME" "$FILE_URL"
+
+# Check if dowloading completed succesfull
+if [$? -eq 0]; then
+	echo "FILENAME downloaded succesfully to $DESTINATION_PATH."
+else
+	echo "Error downloading file $FILENAME."
+	exit 1
+fi 
 
 # Install required packages if they are not installed
 echo "Checking and installing required packages..."
 
 # Install disko
 sudo nix-env -iA nixos.disko
+
+# Download disko-config.nix
 
 # Run disko
 echo "Running disko..."
